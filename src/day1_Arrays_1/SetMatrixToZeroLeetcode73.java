9c22a8bf36eeb5d40e957df94ef3d441a1e9073e
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-06-16 08:02:47 +0530

    Added array_1 SetMatrixToZero solution

---
+++ src/day1_Arrays_1/SetMatrixToZeroLeetcode73.java
@@ -0,0 +1,32 @@
+package day1_Arrays_1;
+
+import java.util.Arrays;
+
+public class SetMatrixToZeroLeetcode73 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setZeroes(int[][] matrix) {
+		int m = matrix.length, n = matrix[0].length, k = 0;
+		// First row has zero?
+		while (k < n && matrix[0][k] != 0)
+			++k;
+		// Use first row/column as marker, scan the matrix
+		for (int i = 1; i < m; ++i)
+			for (int j = 0; j < n; ++j)
+				if (matrix[i][j] == 0)
+					matrix[0][j] = matrix[i][0] = 0;
+		// Set the zeros
+		for (int i = 1; i < m; ++i)
+			for (int j = n - 1; j >= 0; --j)
+				if (matrix[0][j] == 0 || matrix[i][0] == 0)
+					matrix[i][j] = 0;
+		// Set the zeros for the first row
+		if (k < n)
+			Arrays.fill(matrix[0], 0);
+	}
+
+}
