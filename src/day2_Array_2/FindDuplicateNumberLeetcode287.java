2b7c056dc841f8579c84286dbfff8d4293b905e2
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-06-18 23:03:54 +0530

    Added Day2 solution 287

---
+++ src/day2_Array_2/FindDuplicateNumberLeetcode287.java
@@ -0,0 +1,21 @@
+package day2_Array_2;
+
+public class FindDuplicateNumberLeetcode287 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int findDuplicate(int[] nums) {
+		for (int n : nums) {
+			if (nums[Math.abs(n)] < 0) {
+				return Math.abs(n);
+			} else {
+				nums[Math.abs(n)] *= -1;
+			}
+		}
+		return -1;
+	}
+
+}
